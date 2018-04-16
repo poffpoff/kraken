91ba5224ea9a392b7c442faec577f8e30c4e0702
napzoux <napzoux@gmail.com>
2018-04-16 20:07:05 +0200

    setup django

--- kraken/wsgi.py
+++ kraken/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kraken1 project.
+WSGI config for kraken project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kraken1.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kraken.settings")
 
 application = get_wsgi_application()
