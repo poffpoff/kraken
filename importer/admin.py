94e733ed0b905e1e5717833b45fbfebe4832b58c
napzoux <napzoux@gmail.com>
2018-04-17 16:19:06 +0200

    add value and data model

--- importer/admin.py
+++ importer/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from importer.models import Pair
+from importer.models import Pair, Data, Value
+
+class ValueInline(admin.TabularInline):
+    model = Value
+
+class DataInline(admin.TabularInline):
+    model = Data
+
+
+class ValueAdmin(admin.ModelAdmin):
+    inlines = [
+        DataInline,
+    ]
 
 # Register your models here.
+admin.site.register(Value, ValueAdmin)
 admin.site.register(Pair)
--- importer/models.py
+++ importer/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class Pair(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Value(models.Model):
+    name = models.CharField(max_length=200)
+    pair = models.ForeignKey(Pair, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Data(models.Model):
+    value = models.ForeignKey(Value, on_delete=models.CASCADE)
+    data = models.FloatField()
+    time = models.DateTimeField()
