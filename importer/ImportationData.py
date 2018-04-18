f1f29db94e370e32aad911d13a88ac304b280373
napzoux <napzoux@gmail.com>
2018-04-18 18:45:56 +0200

    Import des données + CVS pour bitcoins Next step : BDD

--- importer/ImportationData.py
+++ importer/ImportationData.py
@@ -1,7 +1,8 @@
 
 import pandas as pd
 import krakenex
-
+import pprint
+from requests.exceptions import HTTPError
 import datetime
 import calendar
 import time
@@ -13,15 +14,34 @@ def date_nix(str_date):
 # takes nix time and returns date
 def date_str(nix_time):
     return datetime.datetime.fromtimestamp(nix_time).strftime('%m, %d, %Y')
-
+titre =  {'1': ["price"], '2': ["time"], '3':["buy/sell"], '4':["market/limit"], '5':["miscellaneous"]}
+data1 = []
+data2 = []
+data3 = []
 # return formatted TradesHistory request data
-def data(start, end, ofs):
-    req_data = {'type': 'all',
-                'trades': 'true',
-                'start': str(date_nix(start)),
-                'end': str(date_nix(end)),
-                'ofs': str(ofs)
-                }
-    return req_data
+#def data(start, end, ofs):
+#    req_data = {'type': 'all',
+#                'trades': 'true',
+#                'start': str(date_nix(start)),
+#                'end': str(date_nix(end)),
+#                'ofs': str(ofs)
+#                }
+#   return req_data
 
 k = krakenex.API()
+
+try:
+   response = k.query_public('Trades', {'pair': 'XBTEUR', 'since': '1524059916390775696'})
+   pprint.pprint(response['result']['XXBTZEUR'][1])
+except HTTPError as e:
+    print(str(e))
+
+
+data1.append(pd.DataFrame.from_dict(response['result']['XXBTZEUR']))
+#data2.append(pd.DataFrame(data=titre))
+#data3=set().union(data2,data1)
+trades = pd.DataFrame
+trades = pd.concat(data1, axis = 0)
+trades.rename(columns={0: 'price', 1: 'time', 2: 'buy/sell', 3: 'market/limit', 4: 'miscellaneous'}, inplace=True)
+trades.to_csv('data.csv',sep = ';')
+
